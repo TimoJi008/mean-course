455fd52ed19ff6c00811ed9c3e70122deaac2214
Timoj <timo.ji008@gmail.com>
2025-04-29 02:09:29 +0200

    added observable and remover event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Post } from './posts/post.model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  storedPosts: Post[] = [];
-
-  onPostAdded(post) {
-    this.storedPosts.push(post);
-  }
-}
+export class AppComponent {}
