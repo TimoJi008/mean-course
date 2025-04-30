e3944cdb4ec57ec293113fcedbff3929b902a0ec
Timoj <timo.ji008@gmail.com>
2025-04-30 10:21:06 +0200

    fetching iniial posts

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.get("/favicon.ico", (req, res) => res.status(204));
+app.get("/favicon.ico", (req, res) => res.status(204)); // Ignore favicon requests and prevents from loading twice
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("Hello from express!!!!!");
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "aaa123",
+      title: "First serve-side post",
+      content: "This is coming from the server",
+    },
+    {
+      id: "bbb123",
+      title: "Second server-side post",
+      content: "This is coming from the server!",
+    },
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully!",
+    posts: posts,
+  });
 });
 
 module.exports = app;
