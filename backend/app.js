59a04b77e4d1d66e18cae5c4734e39c3803daa00
Timoj <timo.ji008@gmail.com>
2025-05-02 06:39:45 +0200

    created a Post instance

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const Post = require("./models/post"); // Import the Post model
+
 const app = express();
 
 app.use(bodyParser.json()); // Parse JSON data from incoming requests
@@ -22,7 +24,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
   console.log(post);
   res.status(201).json({
     message: "Post added successfully!",
