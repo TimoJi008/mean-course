9ffaf0ed4fb2458eb0a8000183c82002f2934c77
Timoj <timo.ji008@gmail.com>
2025-05-04 16:35:44 +0200

    added delete post

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -43,4 +43,12 @@ export class PostsService {
         this.postUpdated.next([...this.posts]);
       });
   }
+
+  deletePost(postId: string) {
+    this.http
+      .delete('http://localhost:3000/api/posts/' + postId)
+      .subscribe(() => {
+        console.log('Deleted!');
+      });
+  }
 }
